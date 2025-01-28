6eca4ba5c03e1f9bf703515edea8f852fe887965
MonaRK <mondadov@gmail.com>
2025-01-28 19:03:28 +0100

    novy subor prva verzia

---
+++ main.py
@@ -0,0 +1,27 @@
+
+subor = input('Aky subor chces otvorit? ')
+try:
+    with open(subor, 'r') as citam:
+        while True:
+            content = citam.readlines()
+            if not content:
+                break
+            print(content)
+except FileNotFoundError:
+    print('File not found')
+
+zapis_text = input('Aky text chces zapisat? ')
+try:
+    with open('text1.txt', 'a') as zapis:
+        zapis.write(zapis_text + '\n')
+except Exception as e:
+    print('An error occurred:', e)
+
+
+
+
+
+
+
+
+
